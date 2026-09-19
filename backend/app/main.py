80b2a946f9919df6d706c4f53a14f60b50db3acf
shlok208 <shlokthakkar208@gmail.com>
2026-09-19 11:56:18 +0530

    little change

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Lead Management API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
